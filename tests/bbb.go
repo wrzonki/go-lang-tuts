dde36bc548d2091fd197d7aacc7004e28743395f
wrzonki <wrzonki@gmail.com>
2022-04-25 01:57:23 +0200

    tests

---
+++ tests/bbb.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	// tell time
+	start := time.Now()
+	fmt.Println("Start:", start)
+}
--- tests/main.go
+++ tests/main.go
@@ -1,27 +1,27 @@
 package main
 
 import (
-	"fmt"
-	"sort"
+  "fmt"
+  "sort"
 )
 
 func main() {
-	mySlice := []int{8, 1, 2, 3, 4, 5}
-	fmt.Println(add(mySlice...))
+  mySlice := []int{8, 1, 2, 3, 4, 5}
+  fmt.Println(add(mySlice...))
 
-	sortNumbers(mySlice)
-	fmt.Println(mySlice)
+  sortNumbers(mySlice)
+  fmt.Println(mySlice)
 }
 
 func add(numbers ...int) int {
-	sum := 0
-	for _, num := range numbers {
-		sum += num
-	}
-	return sum
+  sum := 0
+  for _, num := range numbers {
+    sum += num
+  }
+  return sum
 }
 
 //sort slice of numbers
 func sortNumbers(numbers []int) {
-	sort.Ints(numbers)
+  sort.Ints(numbers)
 }
