68c212d1d54c61b36c3acc66dd6c78a5bdfe4121
wrzonki <wrzonki@gmail.com>
2022-05-19 21:57:43 +0200

    slices 🍕

--- main.go
+++ main.go
@@ -43,3 +43,49 @@ func addNamedVariables(a int, b int) (x, y int) {
 func divide(a, b int) (int, int) {
 	return a / b, a % b
 }
+
+func simpleLoop(n int) int {
+	sum := 0
+	for i := 0; i < n; i++ {
+		sum += i + 1
+	}
+	return sum
+}
+
+func simpleSwitch(n int) int {
+	switch n {
+	case 1:
+		return 1
+	case 2:
+		return 2
+	case 3:
+		return 3
+	}
+	return 0
+}
+
+func simple2dArray() [5][5]int {
+	var arr = [5][5]int{}
+	for i := 0; i < 5; i++ {
+		for j := 0; j < 5; j++ {
+			arr[i][j] = i + j
+		}
+	}
+	return arr
+}
+
+func simpleSlice() (p1 int, p2 int) {
+	var arr = []int{}
+	arr = append(arr, 1)
+	arr = append(arr, 2)
+	arr = append(arr, 3)
+	arr = append(arr, 4)
+	arr = append(arr, 5)
+	arr = append(arr, 6)
+	var slice1 = arr[3:4]
+	var slice2 = arr[:3]
+	p1 = slice1[0]
+	p2 = slice2[2]
+
+	return p1, p2
+}
--- main_test.go
+++ main_test.go
@@ -53,3 +53,56 @@ func Test_divide(t *testing.T) {
 		}
 	})
 }
+
+func Test_simpleLoop(t *testing.T) {
+	got := simpleLoop(3)
+	want := 6
+	if got != want {
+		t.Error("Expected", got, "wanted", want)
+	}
+}
+
+func Test_simpleSwitch(t *testing.T) {
+	got1 := simpleSwitch(1)
+	want1 := 1
+	if got1 != want1 {
+		t.Error("Expected", got1, "wanted", want1)
+	}
+
+	got2 := simpleSwitch(2)
+	want2 := 2
+	if got2 != want2 {
+		t.Error("Expected", got2, "wanted", want2)
+	}
+
+	got3 := simpleSwitch(3)
+	want3 := 3
+	if got3 != want3 {
+		t.Error("Expected", got3, "wanted", want3)
+	}
+
+	got4 := simpleSwitch(1111)
+	want4 := 0
+	if got4 != want4 {
+		t.Error("Expected", got4, "wanted", want4)
+	}
+}
+
+func Test_simple2dArray(t *testing.T) {
+	got := simple2dArray()[1][1]
+	want := 2
+	if got != want {
+		t.Error("Expected", got, "wanted", want)
+	}
+}
+
+func Test_simpleSlice(t *testing.T) {
+	got1, got2 := simpleSlice()
+	want1, want2 := 4, 3
+	if got1 != want1 {
+		t.Error("Expected", got1, "wanted", want1)
+	}
+	if got2 != want2 {
+		t.Error("Expected", got2, "wanted", want2)
+	}
+}
